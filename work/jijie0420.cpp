55718ced5f67d3866ce4d9a023bc2c33a2a93345
unknown <3111286105@qq.com>
2023-04-20 11:12:43 +0800

    commit jijie0420.cpp

---
+++ work/jijie0420.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	if(n==1){
+		cout<<"Spring";
+	}else if(n==2){
+		cout<<"Summer";
+	}else if(n==3){
+		cout<<"Fall";
+	}else if(n==4){
+		cout<<"Winter";
+	}
+	return 0;
+} 
