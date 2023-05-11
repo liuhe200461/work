bcb54a9ee3985eaed74040fe73d4e629bba7781e
unknown <3111286105@qq.com>
2023-05-11 15:15:48 +0800

    commit biaozhundianhuahaoma0511.cpp

---
+++ work/biaozhundianhuahaoma0511.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char a[7];
+	int i=0;
+	for(;i<7;i++){
+		cin>>a[i];
+	}
+	for(i=0;i<7;i++){
+		if(a[i]=='A'||a[i]=='B'||a[i]=='C'){
+			cout<<"2";
+		}else if(a[i]=='D'||a[i]=='E'||a[i]=='F'){
+			cout<<"3";
+		}else if(a[i]=='G'||a[i]=='H'||a[i]=='I'){
+			cout<<"4";
+		}else if(a[i]=='J'||a[i]=='K'||a[i]=='L'){
+			cout<<"5";
+		}else if(a[i]=='M'||a[i]=='N'||a[i]=='O'){
+			cout<<"6";
+		}else if(a[i]=='P'||a[i]=='Q'||a[i]=='R'||a[i]=='S'){
+			cout<<"7";
+		}else if(a[i]=='T'||a[i]=='U'||a[i]=='V'){
+			cout<<"8";
+		}else if(a[i]=='W'||a[i]=='X'||a[i]=='Y'||a[i]=='Z'){
+			cout<<"9";
+		}
+	}
+	return 0;
+}
