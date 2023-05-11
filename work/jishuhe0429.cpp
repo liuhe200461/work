b610b1e374fa20b876c8ae0ce6b42a825fd0cb4d
unknown <3111286105@qq.com>
2023-05-11 08:54:17 +0800

    commit jishuhe0429.cpp

--- work/jishuhe0429.cpp
+++ work/jishuhe0429.cpp
@@ -1,34 +1,23 @@
 #include <iostream>
 using namespace std;
-class jishuhe{
-	int a[10];
-	int i;
-	int s=0;
-	public:
-		void even(){
-			for(i=0;i<10;i++){
-				if(a[i]<1){
-					break;
-				}else if(a[i]%2==1){
-					a[i]==a[i];
-				}else if(a[i]%2==0){
-					a[i]=0;
-				}
-			}
-		}
-		void qiuhe(){
-			for(i=0;i<10;i++){
-				s=s+a[i];
-			}
-		}
-};
-int main(){
-	int a[10];
-	int i;
-	for(i=0;i<10;i++){
-		cin>>a[i];
-	}
-	jishuhe kk;
-	kk.even();
-	kk.qiuhe();
+
+int even(int n) {
+    if (n % 2 == 0)
+        return 1;
+    else
+        return 0;
 }
+
+int main() {
+    int num, sum = 0;
+    while (true) {
+        cin >> num;
+        if (num <= 0) break;
+        if (even(num) == 0) sum += num; // 调用 even 函数进行判断
+    }
+    cout << sum << endl;
+    return 0;
+}
+
+
+
